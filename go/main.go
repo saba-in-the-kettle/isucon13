8434971961abfc660a6c2658fc18e2aea6cbfab8
Arata Sato <aratsato@gmail.com>
2023-11-25 13:29:13 +0900

    reaction改善 24412

--- go/main.go
+++ go/main.go
@@ -151,6 +151,10 @@ func initializeHandler(c echo.Context) error {
 		c.Logger().Errorf("create image hash failed with err=%s", err)
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to initialize: "+err.Error())
 	}
+	if err := isuutil.CreateIndexIfNotExists(dbConn, "create index reaction_livestream_id_index\n    on reactions (livestream_id);\n\n"); err != nil {
+		c.Logger().Errorf("create reaction_livestream_id_index failed with err=%s", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to initialize: "+err.Error())
+	}
 
 	if out, err := exec.Command("../pdns/init_zone.sh").CombinedOutput(); err != nil {
 		c.Logger().Warnf("init.sh failed with err=%s", string(out))
