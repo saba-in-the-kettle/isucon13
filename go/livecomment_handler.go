3dbc63253db69ee529f4358563ebc24061b2d4e7
p1ass <me@p1ass.com>
2023-11-25 15:38:15 +0900

    hitspamの判定をGoで

--- go/livecomment_handler.go
+++ go/livecomment_handler.go
@@ -235,20 +235,11 @@ func postLivecommentHandler(c echo.Context) error {
 
 	var hitSpam int
 	for _, ngword := range ngwords {
-		query := `
-		SELECT COUNT(*)
-		FROM
-		(SELECT ? AS text) AS texts
-		INNER JOIN
-		(SELECT CONCAT('%', ?, '%')	AS pattern) AS patterns
-		ON texts.text LIKE patterns.pattern;
-		`
-		if err := tx.GetContext(ctx, &hitSpam, query, req.Comment, ngword.Word); err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, "failed to get hitspam: "+err.Error())
-		}
-		c.Logger().Infof("[hitSpam=%d] comment = %s", hitSpam, req.Comment)
-		if hitSpam >= 1 {
-			return echo.NewHTTPError(http.StatusBadRequest, "このコメントがスパム判定されました")
+		if strings.Contains(req.Comment, ngword.Word) {
+			c.Logger().Infof("[hitSpam=%d] comment = %s", hitSpam, req.Comment)
+			if hitSpam >= 1 {
+				return echo.NewHTTPError(http.StatusBadRequest, "このコメントがスパム判定されました")
+			}
 		}
 	}
 
