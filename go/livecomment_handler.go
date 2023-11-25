ba710f8a6b6eed77825c88868a5d37e3d1fd5049
p1ass <me@p1ass.com>
2023-11-25 16:31:01 +0900

    livestream_id でwhereする

--- go/livecomment_handler.go
+++ go/livecomment_handler.go
@@ -406,7 +406,7 @@ func moderateHandler(c echo.Context) error {
 
 	// ライブコメント一覧取得
 	var livecomments []*LivecommentModel
-	if err := tx.SelectContext(ctx, &livecomments, "SELECT * FROM livecomments"); err != nil {
+	if err := tx.SelectContext(ctx, &livecomments, "SELECT * FROM livecomments WHERE livestream_id = ?", livestreamID); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get livecomments: "+err.Error())
 	}
 	ngedCommentIDs := make([]int64, 0)
